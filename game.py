3dddb3735fab80ff66a1075a646d16c38e04d381
AldoHadid <62579271+AldoHadid@users.noreply.github.com>
2022-12-14 21:55:25 +0700

    Add files via upload
    membuat class game untuk diakses client dan server

---
+++ game.py
@@ -0,0 +1,66 @@
+# Buat Class Game untuk diakses client sama server
+class Game:
+    def __init__(self, id):
+
+        # Player 1 sudah memilih atau player 2 sudah memilih
+        self.p1Went = False
+        self.p2Went = False
+        self.ready = False
+
+        # Current game id
+        self.id = id
+
+        # Currently moves are none
+        self.moves = [None, None]
+
+        self.wins = [0,0]
+        self.ties = 0
+
+    # 0 untuk player1, 1 untuk player2
+    def get_player_move(self, p):
+        """
+        :param p: [0,1]
+        :return: Move
+        """
+        return self.moves[p]
+
+    # Mengupdate Currently moves
+    def play(self, player, move):
+        self.moves[player] = move
+        if player == 0:
+            self.p1Went = True
+        else:
+            self.p2Went = True
+
+    # Kalo siap akan mengupdate di bagian server
+    def connected(self):
+        return self.ready
+
+    # Kalo kedua pemain meninggalkan permainan
+    def bothWent(self):
+        return self.p1Went and self.p2Went
+
+    def winner(self):
+
+        p1 = self.moves[0].upper()[0]
+        p2 = self.moves[1].upper()[0]
+        # Jika P1 menang maka 0, jika P2 menang maka 1, jika seri makan -1
+        winner = -1
+        if p1 == "R" and p2 == "S":
+            winner = 0
+        elif p1 == "S" and p2 == "R":
+            winner = 1
+        elif p1 == "P" and p2 == "R":
+            winner = 0
+        elif p1 == "R" and p2 == "P":
+            winner = 1
+        elif p1 == "S" and p2 == "P":
+            winner = 0
+        elif p1 == "P" and p2 == "S":
+            winner = 1
+
+        return winner
+
+    def resetWent(self):
+        self.p1Went = False
+        self.p2Went = False
